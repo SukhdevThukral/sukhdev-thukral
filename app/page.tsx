affa1dd4be904de71e93eb228d35068f412c6f7c
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-03-10 07:55:14 +0530

    feat: enhance layout and styling of contact and introduction sections
    - Adjust header alignment and structure for better layout consistency
    - Update text sizing and scaling for responsive design
    - Improve readability of bio text with max-width and line breaks

--- app/page.tsx
+++ app/page.tsx
@@ -12,20 +12,22 @@ export default function Home(){
   return(
     <>
     <div className="relative min-h-screen">
-      <div className={`flex flex-row gap-2 absolute top-2 left-3 text-lg font-bold ${nimbusSans.className}`}>
-        <h1 className="italic">INDEX,</h1>
-        <h1> ABOUT</h1>
-      </div>
-      <div className={`absolute top-2 right-3 text-lg font-bold ${nimbusSans.className}`}> 
+      <div className={`flex justify-between items-center absolute top-2 left-3 right-3 text-lg font-bold ${nimbusSans.className}`}>
+        <div className=" flex gap-2">
+          <h1 className="italic">INDEX,</h1>
+          <h1> ABOUT</h1>
+        </div>
         <h1 className="underline underline-offset-7">CONTACT</h1>
       </div>
     
       <div className="flex flex-col justify-center items-center h-screen px-4">
-        <div className={`flex flex-col text-center text-[6em] sm:text-[6em] md:text-[10em] lg:text-[16em] text-[#EA3424] font-bold leading-[1] ${YingHei.className}`}>
-          <span>SUKHDEV</span>
-          <span>THUKRAL</span>
-        </div>
-        <div className={`text-center max-w-5xl w-full text-[0.9rem] sm:text-[1rem] md:text-[1.125rem] lg:text-[1.2rem] text-[#000000] font-bold ${nimbusSans.className}`}>I AM A HIGHSCHOOL SENIOR AND ALSO A SELF PROCLAIMED PRODUCT ENGINEER :3 LIVING IN INDIA. I LOVE MAKING THINGS (PRODUCTS).THE PROCESS OF TURNING AN IDEA FROM A COLLECTION OF THOUGHTS INTO SOMETHING THAT ACTUALLY EXISTS IS SUPER FUN. ITS WHAT I GET TO SPEND MY DAYS DOING AND I ABSOLUTELY LOVE IT</div>
+        {/* <div className="flex flex-col items-center w-full max-w-[50vw]"> */}
+          <div className={`flex flex-col text-center text-[18.4vw] sm:text-[18.6vw] md:text-[18.8vw] lg:text-[19vw] text-[#EA3424] font-bold leading-[0.9] tracking-tight ${YingHei.className}`}>
+            <span>SUKHDEV</span>
+            <span>THUKRAL</span>
+          </div>
+          <div className={`text-center w-fit max-w-[100ch] text-[0.9rem] sm:text-[1.13rem] md:text-[1.125rem] lg:text-[1.1rem] text-[#000000] font-bold ${nimbusSans.className}`}>I AM A HIGHSCHOOL SENIOR AND ALSO A SELF PROCLAIMED PRODUCT ENGINEER :3 LIVING IN INDIA. I LOVE MAKING THINGS (PRODUCTS).THE PROCESS OF TURNING AN IDEA FROM A COLLECTION OF THOUGHTS INTO SOMETHING THAT ACTUALLY EXISTS IS SUPER FUN. ITS WHAT I GET TO SPEND MY DAYS DOING AND I ABSOLUTELY LOVE IT</div>
+        {/* </div> */}
       </div>
     </div>
     </>
