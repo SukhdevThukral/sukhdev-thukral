5424ce3689ee4e9d856684134f045bcc205223f3
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-03-14 17:16:56 +0530

    feat: enhance HoverImage component with portal rendering and mounting check

--- app/components/hoverRow.tsx
+++ app/components/hoverRow.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { createPortal } from "react-dom";
 
 type HoverImageProps = {
     imgSrc: string;
@@ -11,6 +12,11 @@ type HoverImageProps = {
 
 export default function HoverImage({imgSrc, active}: HoverImageProps){
     const container = useRef<HTMLDivElement>(null);
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(()=>{
+        setMounted(true);
+    }, []);
 
     useGSAP(() => {
         if (!container.current) return;
@@ -43,12 +49,14 @@ export default function HoverImage({imgSrc, active}: HoverImageProps){
         return() => window.removeEventListener("mousemove", moveImage);
     }, [active]);
 
-
-    return(
-        <div ref={container} className="fixed top-0 left-0 w-80 h-52 pointer-events-none z-[10000] opacity-0 scale-0 overflow-hidden bg-[#EA3424] p-3 shadow-2xl">
+    if(!mounted) return null;
+    //"teleporting this div to the doc body"
+    return createPortal(
+        <div ref={container} className="fixed top-0 left-0 w-80 h-52 pointer-events-none z-[10000] opacity-0 scale-0 overflow-hidden bg-[#EA3424] p-3 shadow-2xl"> 
             <div className="relative w-full h-full overflow-hidden">
                 <img src={imgSrc} alt=" preview" className="w-full h-full object-cover"/>
             </div>
-        </div>
+        </div>,
+        document.body
     );
 }
