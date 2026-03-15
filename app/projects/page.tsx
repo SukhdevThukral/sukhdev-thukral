e3cec3a74a7e838e25c5ef123f05334b856320c0
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-03-15 16:33:00 +0530

    feat: enhance project page layout and add custom cursor

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
+import CustomCursor from "../components/customCursor";
 import "bootstrap-icons/font/bootstrap-icons.css"
 import localFont from "next/font/local";
 
@@ -53,6 +54,8 @@ const PROJECTS = [
 
 export default function projectPage() {
     return(
+        <div>
+        <CustomCursor/>
         <main className="min-h-screen bg-[#F6F4F2] text-black px-6 md:px-16 md:py-20 font-sans">
             <div className="flex items-center gap-2 mb-5">
                 
@@ -62,14 +65,46 @@ export default function projectPage() {
                 </span>
             </div>
 
-            <header className="flex flex-col lg:flex-row justify-between items-start mb-24 border-b border-black/5 pb-16">
-                <h1 className={`text-[15vw] md:text-[9vw] leading-[0.8] tracking-tighter ${articulat.className}`}>
-                    Projects.
+            <header className="flex flex-col lg:flex-row justify-between items-start mb-5 border-b border-black/5 pb-16">
+                <h1 className={`text-[15vw] md:text-[6vw] leading-[0.8] tracking-tighter ${articulat.className}`}>
+                    XPerience.
                 </h1>
                 <div className="max-w-[280px] mt-8 lg:mt-auto">
-                    <p className="text-sm"></p>
+                    <p className="text-sm leading-relaxed text-gray-600/70 font-medium">
+                        This aim to give a glimpse to the stuff like programming, robotics and etc ive done throughout my life iguess?
+                    </p>
                 </div>
             </header>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-24">
+                {PROJECTS.map((project) => (
+                    <div key={project.id} className="group cursor-pointer">
+                        <div className="relative aspect-[1.8/1] overflow-hidden bg-neutral-200">
+                            <Image
+                                src={project.image}
+                                alt={project.title}
+                                fill className="object-cover transition-transform duration-700 group-hover:scale-105"
+                            />
+                        </div>
+                        <div className="mt-6 grid grid-cols-[1fr_2fr_1fr] items-start items-start border-t border-black/10 pt-4">
+                            <span className={`"text-[11px] font-black tracking-tighter" ${articulat.className}`}>
+                                {project.id}
+                            </span>
+                            <div className="text-center">
+                                <h3 className={`"text-sm font-bold leading-none mb-1 tracking-tighter" ${articulat.className}`}>
+                                    {project.title}
+                                </h3>
+                                <p className="text-[12px] text-neutral-400 font-medium italic">
+                                    {project.category}
+                                </p>
+                            </div>
+                            <span className={`"text-[11px] font-black tracking-tighter text-right " ${articulat.className}`}>
+                                {project.year}
+                            </span>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </main>
-    )
+        </div>
+    );
 }
