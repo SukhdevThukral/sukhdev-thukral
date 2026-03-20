4867008968cfdae95a2177828285b8c8f165ff73
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-03-20 17:50:38 +0530

    feat: add custom fonts and update page layout

--- app/misc/page.tsx
+++ app/misc/page.tsx
@@ -19,6 +19,14 @@ const probaMediumItalic = localFont({
     src: "/fonts/probaMediumItalic.otf"
 })
 
+const abnormal = localFont({
+    src: "/fonts/AbnormalSemiBold.otf"
+})
+
+const probaRegular = localFont({
+    src: "/fonts/probaRegular.otf"
+})
+
 export default function miscPage() {
     const container = useRef<HTMLDivElement>(null);
     const scrollTrack = useRef<HTMLDivElement>(null);
@@ -39,29 +47,38 @@ export default function miscPage() {
 
     return(
         <main ref={container} className="bg-[#F6F4F2] overflow-hidden font-sans selection:bg-[#EA3424] selection:text-white">
-            <nav className="fixed top-0 left-0 w-full z-50 py-12 px-20 flex justify-between items-center mix-blend-multiply">
-                <div className="flex gap-12 md:gap-20 font-bold text-[11px] uppercase tracking-wdiest text-[#EA3424] "></div>
-                {["2020", "2022", "2024", "2026", "FUTURE"].map((year) => (
-                    <button key={year} className="hover:opacity-50 transition-opacity">
-                        {year}
-                    </button>
-                ))}
+
+            <nav className="fixed top-0 left-0 w-full z-50 py-8 pointer-events-none">
+                <div className="max-w-6xl mx-auto px-12 flex justify-between items-center pointer-events-auto text-[#EA3424]">
+                    {["2020", "2022", "2024", "2026", "FUTURE"].map((year) => (
+                        <button key={year} className="text-[14px] uppercase tracking-[0.4em] hover:opacity-50 transition-all">
+                            {year}
+                        </button>
+                    ))}
+                </div>
             </nav>
 
             <div ref={scrollTrack} className="flex w-[500vw] h-screen">
-                <section className="panel w-screen h-screen flex flex-col justify-center px-24 relative">
-                    <div className="max-w-7xl mx-auto w-full flex flex-col justify-center">
-                        <span className="text-3xl font-bold mb-2 block leading-none text-[#EA3424]">A retrospective on</span>
+                <section className="panel w-screen h-screen flex flex-col pt-40 relative">
+
+                    <div className="max-w-[1216px] mx-auto w-full px-12 flex flex-col">
+
+                        <span className={`text-4xl font-bold mb-[-1] block leading-none text-[#EA3424] ${abnormal.className}`}>
+                            A retrospective on
+                        </span>
 
                         <div className="relative w-full -ml-8">
                             <img src="https://cdn.hackclub.com/019d09f8-57b4-7e9b-afe1-2cbb992051b6/coolestboy.webp"
                              alt="cutie" 
-                             className="w-full h-auto max-w-[90vw] object-contain select-none pointer-events-none"/>
+                             className="w-full h-auto max-w-[50vw] object-contain select-none pointer-events-none"/>
                         </div>
-                        <div className="flex justify-end pr-10 mt-8">
-                            <div className="max-w-md">
-                                <h3 className={`text-4xl mb-6 tracking-tight ${probaMediumItalic.className}`}>a short intro :3</h3>
-                                <p className={`text-xl leading-relaxed opacity-90 ${probaMedium.className}`}>
+
+                        <div className="flex pr-20 mt-12 text-[#EA3424]">
+                            <div className="max-w-md ml-auto">
+                                <h3 className={`text-4xl mb-6 tracking-tight ${probaMediumItalic.className}`}>
+                                    a short intro :3
+                                </h3>
+                                <p className={`text-xl leading-relaxed opacity-90 font-light ${probaRegular.className}`}>
                                     the only rsn i was able to develop a keen interest for TECHNOLOGY
                                     was by exploring a variety of activites, and sometimes even irrelevant ones,
                                      ive tried my best to mention most of them ALTHO IVE missed sm on purpose which had
@@ -69,8 +86,10 @@ export default function miscPage() {
                                 </p>
                             </div>
                         </div>
+
                     </div>
                 </section>
+
             </div>
         </main>
     )}
